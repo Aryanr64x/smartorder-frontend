ee298ab9050da83d845fc5c02cb40cb2bfa8169b
Aryanr64x <aryansaket@yahoo.com>
2026-01-28 00:50:44 +0530

    updated bacend link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:8000/menu", {
+      const res = await axios.post("https://smartorder-backend.vercel.app/menu", {
         query: userMessage.content,
       });
 
